8bab736e33c0ec9170d7d686f66c597e61fb0efb
cykunz <32450513+cykunz@users.noreply.github.com>
2017-10-23 18:29:16 +0200

    Update Network.hpp
    Update based on changes made to Network.cpp

--- Exercise2b/src/Network.hpp
+++ Exercise2b/src/Network.hpp
@@ -2,6 +2,7 @@
 #define NETWORK_H
 #include <iostream>
 #include <vector>
+#include <array>
 #include "Neuron.hpp"
 
 using namespace std;
@@ -9,41 +10,68 @@ using namespace std;
 class Network
 {	
 	private:
-	/*	A network is caracterized by the neurons it contains, its global time, 
+	/*!	A network is caracterized by the neurons it contains, its global time, 
 	 * 	the start and end time of its simulation.	*/
 	 
-	vector<Neuron*> neurons_;	/*	Neurons in network.	*/
-	int clock_time_;	/*	Global time of network.	*/
-	int timestart_;
-	int timeend_;
+	vector<Neuron*> neurons_;	/**<	Neurons in network.	*/
+	unsigned int clock_time_;	/**<	Global time of network.	*/
+	unsigned int timestart_;		/**<	Start time of simulation.	*/
+	unsigned int timeend_;		/**< End time of simulation.	*/
 	
 	public:
-	/*	By default, the vector of neurons it contains has a size of 0, and all times are set to 0. 	*/
-	Network(vector<Neuron*> new_neur=vector<Neuron*>(0), int clock=0, int timestart=0, int timeend=0);
+	//! Constructor
+	/*! By default, the vector of neurons it contains has a size of 0, and all times are set to 0.*/
+	Network(vector<Neuron*> new_neur=vector<Neuron*>(0), unsigned int clock=0, unsigned int timestart=0, unsigned int timeend=0);
+	
+	//! Destructor
+	/*!	Clears the vector of neurons in the network by setting them to nullptr and deleting them.	*/
 	~Network();
 
 /***************************************************/
 	/*	Getters	*/
-	int getClockTime() const;
+	//! Gets the clock time (simulation time)
+	/*!	@return Simulation time, as a dt step.	*/
+	unsigned int getClockTime() const;
+	//! Gets the neurons in the network
+	/*!	@return Vector of pointers on neurons.	*/
 	vector<Neuron*> getNeurons() const;
-	int getTimeEnd() const;
-	int getTimeStart() const;
+	//! Gets the end time of simulation
+	/*!	@return End time, as a dt step.	*/
+	unsigned int getTimeEnd() const;
+	//! Gets the start time of simulation
+	/*!	@return Start time, as a dt step.	*/
+	unsigned int getTimeStart() const;
 	
 /***************************************************/
 	/*	Setters	*/
-	void setClockTime(int const& new_time);
+	//!	Sets the clock time
+	/*!	@param	new_time: New value for the clock time. */
+	void setClockTime(unsigned int const& new_time);
+	//!	Sets the neurons in the network
+	/*!	@param	new_neurons: New vector of neurons wanting to be part of the network. */
 	void setNeurons(vector<Neuron*> const& new_neurons);
-	void setTimeStart(int const& new_timestart);
-	void setTimeEnd(int const& new_timeend);
+	//!	Sets the time at which the simulation start
+	/*!	@param	new_timestart= New start time value. */
+	void setTimeStart(unsigned int const& new_timestart);
+	//!	Sets the time at which the simulation ends
+	/*!	@param	new_timeend: New end time value. */
+	void setTimeEnd(unsigned int const& new_timeend);
 
 /***************************************************/
-	/*	Method clearing all neurons in the vector of the network.	*/
+	
+	//!A public function
+	/*!	Clears the vector of all neurons in the network.*/
 	void clearNeurons();
 	
-	/*	Method linking two neurons; each neuron will appear in the other's linked_neurons_ list.	*/
+	//!A public function taking vector of pointers on neurons as a parameter
+	/*!	Links all neurons in the vector given.
+	 * 	@param neurons_to_link: Vector of pointers on neurons wanting to be linked.	*/
 	void createLink(vector<Neuron*> neurons_to_link);
-	
-	/*	Method passing on the spike signal of each neuron to its linked neurons. 	*/	
+
+	//!A public function taking two double arguments
+	/*!	Method updating all neurons in the network, passing on the spike signal in case of spike.	
+	 * 	@param timeA: Time at which simulation starts.
+	 * 	@param timeB: Time at which simulation ends.		*/
 	void update(double const& timeA, double const& timeB);
 	
 	
