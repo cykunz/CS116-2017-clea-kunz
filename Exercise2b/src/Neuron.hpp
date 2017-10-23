95a0eb2975a1a850c1ece7b48ca658da76500692
cykunz <32450513+cykunz@users.noreply.github.com>
2017-10-23 18:42:45 +0200

    Update Neuron.hpp
    External file containing all constants

--- Exercise2b/src/Neuron.hpp
+++ Exercise2b/src/Neuron.hpp
@@ -4,20 +4,9 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 #include <math.h>
+#incldue "Utility/Constants"
 
 using namespace std;
-
-//!	Constants needed for the calculations	*/
-const double dt = 0.1;//0.0001; 					/**<	The time step, 0.1 milliseconds	*/
-const double MembraneThreshold =20.0;//= 0.02; 		/**<	The membrane potential threshold, 20.0 milliVolts	*/
-const double MembraneReset = 0.0; 			/**<	The membrane potential reset value, 0.0 Volts	*/
-const double RefractoryPeriod = 2.0;//0.002; 		/**<	The refractory period, 2 milliseconds	*/
-const double Tao = 20.0;//0.02;
-const double Capacity =  1.0;				/**< 	The capacity, 1 Faraday	*/
-const double Resistance = Tao/Capacity;  /**< The resistance, in Ohms	*/
-const double Amplitude= 0.1;//0.0001;				/**< The amplitude, 0.1 milliVolts	*/
-const double Delay=1.5;//0.0015;					/**< The delay, 1.5 milliseconds	*/
-const unsigned int delay_steps=static_cast<unsigned long>(floor(Delay/dt));
 
 class Neuron {
 	private:	
